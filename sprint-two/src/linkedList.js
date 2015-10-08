6d3bf3ea585b8997b2fb839da36ea4dc44f1b1d4
ahrobinson <ahrobins3@gmail.com>
2015-10-08 17:40:12 -0500

    complete linkedList!

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,34 @@
+
 var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if(list.head === null){
+      list.head = newNode;
+    }
+      list.tail = newNode;
+  
   };
 
   list.removeHead = function(){
+    var stored = list.head;
+    list.head = Node(list.tail.value)
+    return stored.value;
   };
 
   list.contains = function(target){
+    var result = false;
+    _.each(list, function(val){
+      _.each(val, function(item, key){
+        if(item === target){
+          result = true;
+        }
+      })
+    })
+    return result;
   };
 
   return list;
