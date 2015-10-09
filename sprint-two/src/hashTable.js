8e5905cb4e1fcefd5a2ffa2acf53c20b39dc6257
ahrobinson <ahrobins3@gmail.com>
2015-10-08 19:50:47 -0500

    complete hashtable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,19 +1,40 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.bucket = [];
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  if(!this._storage[i]) {
+    this._storage[i] = [[k,v]];
+   } else if (_.contains(this._storage[i][0], k)){
+    this._storage[i][0] = [k,v];
+
+   } else {
+    this._storage[i].push([k,v])
+   }
+
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
-};
+  if(this._storage[i][0][1] === null){
+    return null;
+  }
+  
+  for(var j = 0; j < this._storage[i].length; j++){
+    if (_.contains(this._storage[i][j], k)){
+      return this._storage[i][j][1];
+    }
+  }
+}
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i][0][1] = null;
 };
 
 
