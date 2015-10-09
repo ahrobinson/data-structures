85687c26feac1cb07f496dd4e610bdc4e7257f85
ahrobinson <ahrobins3@gmail.com>
2015-10-08 20:54:12 -0500

    complete tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,33 @@
+
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  this.children.push(Tree(value));
 
 };
 
 treeMethods.contains = function(target){
+
+  if(this.value === target){
+    return true;
+  }
+  for(var i = 0; i < this.children.length; i++){
+    if(this.children[i].contains(target)){
+      return true;
+    }
+  }
+  return false;
 
 };
 
